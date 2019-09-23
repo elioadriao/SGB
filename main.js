a89b56db8950f97cb1e05b670b8f6b408e7c041f
Helio Adriao <elio.adriao@gmail.com>
2019-09-23 08:41:02 -0300

    adicionado toggle para devtools

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
 var Menu = electron.Menu;
+var globalShortcut = electron.globalShortcut;
 
 var mainWindow = null;
 
@@ -17,7 +18,10 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({ width: 1280, height: 720 });
 
   mainWindow.loadURL('file://' + __dirname + '/index.html');
-  mainWindow.webContents.openDevTools();
+
+  globalShortcut.register('CommandOrControl+I', () => {
+    mainWindow.webContents.toggleDevTools();
+  });
 
   mainWindow.on('closed', function() {
     mainWindow = null;
